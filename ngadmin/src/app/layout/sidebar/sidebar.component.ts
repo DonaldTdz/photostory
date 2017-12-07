742e6dfaeffc8ddb67503bfdab4238566b0aca35
tangdezhou <tangdezhou@yun-kai.com>
2017-12-07 18:10:15 +0800

    修改登录信息

--- ngadmin/src/app/layout/sidebar/sidebar.component.ts
+++ ngadmin/src/app/layout/sidebar/sidebar.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import { NzMessageService } from 'ng-zorro-antd';
 import { SettingsService } from '@core/services/settings.service';
+
+import { AppComponentBase } from '@shared/app-component-base';
+import { AppAuthService } from '@shared/auth/app-auth.service';
 
 @Component({
   selector   : 'app-sidebar',
   templateUrl: './sidebar.component.html'
 })
-export class SidebarComponent {
-    constructor(public settings: SettingsService, public msgSrv: NzMessageService) {
+export class SidebarComponent extends AppComponentBase implements OnInit {
+    constructor( injector: Injector, public settings: SettingsService, public msgSrv: NzMessageService, private _authService: AppAuthService) {
+      super(injector);
     }
+
+    ngOnInit() {
+      let user = { name: this.appSession.getShownLoginName(), email: this.appSession.user.emailAddress };
+      this.settings.setUser(user);
+      //this.shownLoginName = this.appSession.getShownLoginName();
+    }
+
+    logout(): void {
+      this._authService.logout();
+  }
 }
