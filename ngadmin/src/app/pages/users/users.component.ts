9bda9cec2fb7d6bb8dbe5565f4094c44dace7f31
tangdezhou <tangdezhou@yun-kai.com>
2017-12-20 17:09:25 +0800

    添加用户

--- ngadmin/src/app/pages/users/users.component.ts
+++ ngadmin/src/app/pages/users/users.component.ts
@@ -1,7 +1,9 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { NzMessageService } from 'ng-zorro-antd';
 import { getRule, saveRule, removeRule } from '../../../../_mock/rule.service';
-import { UserServiceProxy, UserDto, PagedResultDtoOfUserDto } from '@shared/service-proxies/service-proxies';
+import { UserServiceProxy, UserDto, CreateUserDto, RoleDto, PagedResultDtoOfUserDto } from '@shared/service-proxies/service-proxies';
+import { FormGroup, FormBuilder, Validators, FormControl, AsyncValidatorFn, AbstractControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 import { AppComponentBase } from '@shared/app-component-base';
 
@@ -12,7 +14,7 @@ import { AppComponentBase } from '@shared/app-component-base';
 export class UsersComponent extends AppComponentBase implements OnInit {
     q: any = {
         pi: 1,
-        ps: 4,
+        ps: 10,
         total: 0,
         sorter: '',
         status: -1,
@@ -32,27 +34,87 @@ export class UsersComponent extends AppComponentBase implements OnInit {
     sortMap: any = {};
     expandForm = false;
     modalVisible = false;
-    description = '';
+    isConfirmLoading = false;
 
-    constructor(injector: Injector, public msg: NzMessageService, private _userService: UserServiceProxy) {
+    form: FormGroup;
+
+    user: CreateUserDto = null;
+    //roles: RoleDto[] = null;
+    roles: any = [];
+
+    constructor(injector: Injector,private fb: FormBuilder, public msg: NzMessageService, private _userService: UserServiceProxy) {
         super(injector);
     }
 
     ngOnInit() {
         this.refreshData();
+
+        this._userService.getRoles()
+        .subscribe((result) => {
+            this.roles = result.items.map(i => { return { label: i.name, value: i.normalizedName, checked: true }; });
+        });
+
+        this.form = this.fb.group({
+            email: [null, [Validators.email]],
+            password: [null, [Validators.required]],
+            checkPassword: [null, Validators.compose([Validators.required, this.confirmationValidator])],
+            //username: [null, Validators.compose([Validators.required, Validators.minLength(2)]), this.nicknameValidator.bind(this)],
+            username: [null, Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(32)])],
+            name: [null, Validators.compose([Validators.required, Validators.maxLength(32)])],
+            surname: [null, Validators.compose([Validators.required, Validators.maxLength(32)])],
+            isactive: [true],
+            rolegroup: [true]
+        }, );
     }
 
 
     add() {
+        this.user = new CreateUserDto();
+        this.user.init({ isActive: true });
+        this.roles.forEach(element => {
+            element.checked = true;
+        });
         this.modalVisible = true;
-        this.description = '';
     }
 
     save() {
-        this.loading = true;
-        saveRule(this.description);
-        this.refreshData();
-        setTimeout(() => this.modalVisible = false, 500);
+        for (const i in this.form.controls) {
+            this.form.controls[i].markAsDirty();
+        }
+        console.log('log', this.form.value);
+        if (this.form.valid) {
+            this.loading = true;
+            this.isConfirmLoading = true;
+            var roles = [];
+            this.roles.forEach((role) => {
+                if(role.checked){
+                    roles.push(role.value);
+                }
+            });
+        
+            this.user.roleNames = roles;
+            this._userService.create(this.user)
+                .finally(() => { this.isConfirmLoading = false; })
+                .subscribe(() => {
+                    this.notify.info(this.l('SavedSuccessfully'));
+                    //this.msg.success('Successed!');
+                    this.modalVisible = false;
+                    this.refreshData();
+                    this.loading = false;
+                });
+
+        } 
+    }
+
+    handleCancel = (e) => {
+        this.modalVisible = false;
+        this.isConfirmLoading = false;
+        this.loading = false;
+        e.preventDefault();
+        this.form.reset();
+        for (const key in this.form.controls) {
+          this.form.controls[key].markAsPristine();
+        }
     }
 
     remove() {
@@ -68,13 +130,6 @@ export class UsersComponent extends AppComponentBase implements OnInit {
         this.refreshStatus();
     }
 
-    checkAll(value: boolean) {
-        this.curRows.forEach(i => {
-            if (!i.disabled) i.checked = value;
-        });
-        this.refreshStatus();
-    }
-
     refreshStatus() {
         const allChecked = this.curRows.every(value => value.disabled || value.checked);
         const allUnChecked = this.curRows.every(value => value.disabled || !value.checked);
@@ -82,45 +137,58 @@ export class UsersComponent extends AppComponentBase implements OnInit {
         this.indeterminate = (!allChecked) && (!allUnChecked);
         this.selectedRows = this.data.filter(value => value.checked);
         this.totalCallNo = this.selectedRows.reduce((total, cv) => total + cv.callNo, 0);
-    }
-
-    sort(field: string, value: any) {
-        this.sortMap = {};
-        this.sortMap[field] = value;
-        this.q.sorter = value ? `${field}_${value}` : '';
-        this.refreshData();
-    }
-
-    dataChange(res: any) {
-        this.curRows = res;
-        this.refreshStatus();
-    }
+    }  
 
     refreshData(reset = false) {
         if (reset) {
-          this.q.pi = 1;
+            this.q.pi = 1;
         }
         this.loading = true;
-        this._userService.getAll((this.q.pi - 1)*this.q.ps, this.q.ps).subscribe((result: PagedResultDtoOfUserDto) => {
-          this.loading = false;
-          let status = 0;
-          this.data = result.items.map(i => {  
-                  if(i.isActive) {
-                      status = 0;
-                  } else {
-                      status = 1;
-                  }
-                  const statusItem = this.status[status];
-                  i.activeText = statusItem.text;
-                  i.activeType = statusItem.type;
-                  return i;
-              });
-          this.q.total = result.totalCount;
+        this._userService.getAll((this.q.pi - 1) * this.q.ps, this.q.ps).subscribe((result: PagedResultDtoOfUserDto) => {
+            this.loading = false;
+            let status = 0;
+            this.data = result.items.map(i => {
+                if (i.isActive) {
+                    status = 0;
+                } else {
+                    status = 1;
+                }
+                const statusItem = this.status[status];
+                i.activeText = statusItem.text;
+                i.activeType = statusItem.type;
+                return i;
+            });
+            this.q.total = result.totalCount;
         })
-      };
-      
+    };
+
     reset(ls: any[]) {
         for (const item of ls) item.value = false;
         this.refreshData();
+    }
+
+    nicknameValidator = (control: FormControl): Observable<any>  => {
+        return control
+                .valueChanges
+                .debounceTime(500)
+                .map((value) => {
+                    if (value !== 'cipchk') {
+                        control.setErrors({ checked: true, error: true });
+                        return ;
+                    }
+                    control.setErrors(null);
+                });
+    }
+
+    confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
+        if (!control.value) {
+            return { required: true };
+        } else if (control.value !== this.form.controls['password'].value) {
+            return { confirm: true, error: true };
+        }
+    }
+
+    getFormControl(name: string) {
+        return this.form.controls[name];
     }
 }
